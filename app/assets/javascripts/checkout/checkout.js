7664b15aac06319499aadd93fdfca45fd11b9fd6
Florian Kraft <f.kraft@finn.de>
2015-03-05 15:10:09 +0100

    updates the forms to use the new form styling
    also removes a good chunk of prototype based JavaScript functionality
    and displays multiple checkouts simultaneously instead of accessing them
    one after the other.
    
    This was done to simplify the process as well as get rid of old
    functionality that is not really compatible with the new form stuff.
    
    Signed-off-by: Florian Kraft <f.kraft@finn.de>

--- app/assets/javascripts/checkout/checkout.js
+++ app/assets/javascripts/checkout/checkout.js
@@ -1,34 +1,5 @@
 //= require checkout/subform
 
-document.observe("dom:loaded", function() {
-  /* update the checkout URL if clicked on a protocol */
-  var protocols = $('checkout_protocols')
-  if (!protocols) {
-    return;
-  }
-  protocols.select('a').each(function(e) {
-    e.observe('click', function(event) {
-      $('checkout_url').value = checkout_commands.get(this.id);
-      $('checkout_protocols').select('a').each(function(e) {
-        e.removeClassName("selected");
-      });
-      this.addClassName("selected")
-
-      var access = $('checkout_access');
-      if (access) {
-        var value = window.checkout_access.get(this.id);
-        access.innerHTML = value;
-      }
-
-      event.stop();
-    });
-  });
-  /* select the text field contents if activated */
-  Event.observe('checkout_url', 'click', function(event) {
-    this.activate();
-  });
-});
-
 jQuery(document).ready(function() {
   var defaultCheckboxes = jQuery('.checkout_protocol_table .protocol_is_default input[type=checkbox]');
 
